62a684e2096eeb2af0cca09b2ef6d2c99915e51d
Jeremy Carreras <jeremy.carreras@elektra.com.mx>
2026-04-22 12:52:53 -0600

    Adding prod DB

--- server/config/sequelize.conf.ts
+++ server/config/sequelize.conf.ts
@@ -1,16 +1,26 @@
 import './config';
 import { Sequelize, Dialect } from 'sequelize';
 
+const isAiven = (process.env.HOSTDB || '').includes('aivencloud.com');
+
 const sequelize = new Sequelize(
   process.env.DB as string,
   process.env.USERDB as string,
   process.env.PASSWORDDB,
   {
     host: process.env.HOSTDB,
+    port: isAiven ? 28365 : undefined,
     dialect: (process.env.TYPEDB as Dialect) || 'mysql',
     logging: false,
     dialectOptions: {
       useUTC: false,
+      ...(isAiven
+        ? {
+            ssl: {
+              rejectUnauthorized: false,
+            },
+          }
+        : {}),
     },
     timezone: '-05:00',
   }
